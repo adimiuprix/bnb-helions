fdd8f47ba99dbc2dd6a636342714d4c84e1464ed
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-10-06 03:46:20 +0100

    Delete config directory

--- config/config.js
+++
@@ -1,4 +0,0 @@
-export const NFTCONTRACT = "0x4901FBDDf6949736Dc312C514930B56D9A32e7Fa"
-export const STAKINGCONTRACT = "0xBa42A34a37Bc7b1BacE10fCbE045145FF880E9B5"
-export const SOULSCONTRACT = "0x54F808284B582DfaB420240C3E4efFDcF196241E"
-export const IPFSURL = "https://cf-ipfs.com/ipfs/bafybeig3qa67wwlypsfocqbcnwcbrts3hsgosb6wgizvulyy3pr4jpg2fm/"
